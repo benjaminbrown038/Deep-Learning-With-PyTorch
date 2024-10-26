e3f5073579553f1551967cd263f7d2cd639720bb
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 16:49:13 -0400

    Create 2_Generating_Anchor_Boxes.py

---
+++ Week-9/2_Generating_Anchor_Boxes.py
@@ -0,0 +1,12 @@
+from IPython.display import Code
+import inspect
+from trainer.encoder import DataEncoder, decode_boxes, encode_boxes, generate_anchors, generate_anchor_gris
+
+height_width
+data_encoder
+print
+
+height_width
+data_encoder
+print
+
