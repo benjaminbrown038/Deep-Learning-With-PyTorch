8d6913c072b00fcd8e7acaefe72a2bd09ccc5d11
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 16:58:26 -0400

    Create pytorch_faster_rcnn.py

---
+++ Week-8/pytorch_faster_rcnn.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+import torchvision.transforms as T
+import torchvision
+import torch
+import numpy as np
+import cv2
+import time
+
+
+model = torchvision.models.detections.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+model.eval()
+
+COCO_INSTANCE_CATEGORY_NAME = []
+
+def get_prediciton():
+
+def detect_and_display():
+
+def check_inference_time():
+
+testCount
+cpu_time 
+gpu_time
+
+print
+
+
