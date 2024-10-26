7568b6a9dbd972b7c06f109d698c070b4023ce35
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 16:51:55 -0400

    Create non_max_suppresion.py

---
+++ Week-8/non_max_suppresion.py
@@ -0,0 +1,11 @@
+import torch
+
+def soft_nms_rescoring():
+
+def setup_system():
+
+def check_soft_nms():
+
+if __name__ == '__main__':
+    torch.set_printoptions(precision=4)
+    check_soft_nms()
