1b541f30285246d7ebbdbc7310d0334f6d72b18e
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 16:54:14 -0400

    Create average_precision.py

---
+++ Week-8/average_precision.py
@@ -0,0 +1,21 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+def get_iou():
+
+def check_iou_threshold():
+
+def match():
+
+class AveragePrecisionEvaluator:
+    def __init__():
+    def run_ap_calculations():
+    def get_voc_ap():
+    def precision_recall_curve():
+    def visualize_results():
+
+def check_ap():
+
+if __name__ == '__main__':
+    check_ap()
