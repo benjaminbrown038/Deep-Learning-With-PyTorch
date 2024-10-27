9baea1725d864bfff4cefd210cd97ac3b3f7c374
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 22:06:45 -0400

    Create 1_FCN_And_DeepLab_Using_Torchvision.py

---
+++ Week-11/1_FCN_And_DeepLab_Using_Torchvision.py
@@ -0,0 +1,49 @@
+%matplotlib inline
+from torchvision import models
+import torch
+import torchvision.transforms as T
+from PIL import Image
+import matplotlib.pyplot as plt
+import urllib
+import numpy as np
+import time
+
+def download_image():
+
+bird
+download_image()
+img 
+plt.imshow
+plt.show
+
+fcn_resnet101 = model.segmentations.fcn_reset101(pretrained=True).eval()
+cars
+download_image()
+img
+plt.imshow
+plt.show
+
+trf = T.Compose()
+inp 
+print
+
+out 
+print
+im
+print
+print
+
+def decode_segmap():
+    return rgb
+
+rgb = decode_segmap()
+plt.imshow
+
+def segment():
+
+
+cuda_device
+segment()
+
+
+
