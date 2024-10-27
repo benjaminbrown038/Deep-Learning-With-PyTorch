745ddcaf310d693213dc98b3819990f3a81c49a9
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 22:44:19 -0400

    Update 3_From_PyTorch_To_LibTorch.py

--- Week-15/3_From_PyTorch_To_LibTorch.py
+++ Week-15/3_From_PyTorch_To_LibTorch.py
@@ -1,1 +1,23 @@
+import torch
+import torch.nn as nn
 
+script_model = torch.jit.script()
+torch.jit.save()
+
+%cd pytorch-to-libtorch
+
+!ls
+
+!mkdir build
+
+%cd build
+
+! cmake ..
+
+!cmake --build . --config Release
+
+%cd ..
+
+!./build/run-traced-model
+
+  
