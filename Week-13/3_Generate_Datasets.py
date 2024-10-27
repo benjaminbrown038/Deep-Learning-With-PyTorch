ea0b0afdb50c148f714335a258cbec86ce34a809
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-26 23:13:50 -0400

    Create 3_Generate_Datasets.py

---
+++ Week-13/3_Generate_Datasets.py
@@ -0,0 +1,35 @@
+import urllib
+import os
+import json
+import random
+import shutill
+
+def download():
+    return response
+
+denspose_coco_2014_minival_url
+annotations_path
+
+src_folder
+dest_folder
+dest_annotations_folder
+train_dataset
+val_dataset
+
+num_images
+
+os.makedirs
+os.makedirs
+os.makedirs
+
+with open:
+
+count
+train_image_ids
+test_image_ids
+val_image_ids
+
+train_data
+val_data
+test_data
+
